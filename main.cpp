6e081f1e55fdf1105122e4242baa8db41ee0dee0
Igor <igor.muravyov.2015@gmail.com>
2020-03-06 06:03:28 +0200

    Rxx Rxy fixes

--- main.cpp
+++ main.cpp
@@ -16,10 +16,9 @@ using namespace std;
 
 double x_arr[VAR_DISCR];
 double y_arr[VAR_DISCR];
-double Rp_arr[VAR_DISCR];
+double Rxx_arr[VAR_DISCR];
 double Rxy_arr[VAR_DISCR];
 int t_arr[VAR_DISCR];
-
 double harmonic(int t, int w, double ampl, double phi){
 	return ampl*sin(((double)w)*t+phi);
 }
@@ -30,14 +29,16 @@ int main(int argc, char **argv) {
 	appx.init(1024, 768, (char*)"X");
 	appy.init(1024, 768, (char*)"Y");
 	appcorxx.init(1024, 768, (char*)"Rxx");
+	appcorxy.init(1024, 768, (char*)"Rxy");
 	appx.clear_win();
 	double amplx = RNG.get_float(0, 1);
 	double phix = RNG.get_float(0, 1);
-	printf("ampl=%lf, phi=%lf\n", amplx, phix);
+	printf("amplx=%lf, phix=%lf\n", amplx, phix);
 	// here draw line t=0
 	appx.draw_middleline();
 	appy.draw_middleline();
 	appcorxx.draw_middleline();
+	appcorxy.draw_middleline();
 	// find x array
 	double x;
 	for(int t=0; t<VAR_DISCR; t++) {
@@ -52,6 +53,7 @@ int main(int argc, char **argv) {
 
 	double amply = RNG.get_float(0, 1);
 	double phiy = RNG.get_float(0, 1);
+	printf("amply=%lf, phiy=%lf\n", amply, phiy);
 	double y;
 	for(int t=0; t<VAR_DISCR; t++) {
 		y=0;
@@ -85,14 +87,15 @@ int main(int argc, char **argv) {
 	double y_offs = (abs(*(minmaxy.first))>abs(*(minmaxy.second)))?abs(*(minmaxy.first)):abs(*(minmaxy.second));
 	//int t_offs = (abs(*(minmaxt.first))>abs(*(minmaxt.second)))?abs(*(minmaxt.first)):abs(*(minmaxt.second));
 	double y_coef = (appy.end_y() - appy.middle_y()) / y_offs;
-	//double t_coef = (appy.end_x() - appy.middle_x()) / t_offs;
 	for(int i=0; i<VAR_DISCR; i++) {
-		appy.out(t_arr[i]*t_coef, appy.real_y(y_arr[i]*x_coef));
+		appy.out(t_arr[i]*t_coef, appy.real_y(y_arr[i]*y_coef));
 		SDL_SetRenderDrawColor(appy.ren, 10, 150, 0, 0);
 		if(i+1<VAR_DISCR)
-			SDL_RenderDrawLine(appy.ren, t_arr[i]*t_coef, appy.real_y(y_arr[i]*x_coef), (t_arr[i+1]*t_coef), appx.real_y(y_arr[i+1]*y_coef));
+			SDL_RenderDrawLine(appy.ren, t_arr[i]*t_coef, appy.real_y(y_arr[i]*y_coef), (t_arr[i+1]*t_coef), appy.real_y(y_arr[i+1]*y_coef));
 	}
 	appy.refresh_win();
+
+
 	clock_t start_my, end_my, start_dy, end_dy;
 	start_my = clock();
 	double My = Expected(y_arr, VAR_DISCR);
@@ -118,39 +121,58 @@ int main(int argc, char **argv) {
 	printf("mx time=%f\n\r", mx_timeused);
 
 	// get autocorrelation
-	double Rxx_result=0;
-	for(int i0=0, j1=VAR_DISCR; i0<VAR_DISCR; i0++, j1--) {
-		Rp_arr[i0] = 0;
-		for(int i=i0, j=VAR_DISCR-(j1-i0); i<VAR_DISCR; i++, j++) {
-			 double Rp = (x_arr[i] - Mx) * (x_arr[j1] - Mx);
-			 Rp_arr[i0] += Rp;
+	for(int tau=0; tau<VAR_DISCR; tau++) {
+		Rxx_arr[tau] = 0;
+		for(int i=tau; i<VAR_DISCR-tau; i++) {
+			 double Rxx = (x_arr[i] - Mx) * (x_arr[i+tau] - Mx);
+			 Rxx_arr[tau] += Rxx;
 		}
-		Rp_arr[i] /= (VAR_DISCR/2-1);
-		printf("Rxx[%i]=%f\n\r", i0, Rp_arr[i0]);
+		//Rxx_arr[i] /= (VAR_DISCR/2-1);
+		printf("Rxx[%i]=%f\n\r", tau, Rxx_arr[tau]);
 	}
 
 	// draw Rxx(t)
-	std::pair<double*, double*> minmaxRp = std::minmax_element(std::begin(Rp_arr), std::end(Rp_arr));
+	std::pair<double*, double*> minmaxRxx = std::minmax_element(std::begin(Rxx_arr), std::end(Rxx_arr));
 	//std::pair<int*, int*> minmaxt = std::minmax_element(std::begin(t_arr), std::end(t_arr));
 	// conv
-	double Rp_offs = (abs(*(minmaxRp.first))>abs(*(minmaxRp.second)))?abs(*(minmaxRp.first)):abs(*(minmaxRp.second));
-	double Rp_coef = (appcorxx.end_y() - appcorxx.middle_y()) / Rp_offs;
+	double Rxx_offs = (abs(*(minmaxRxx.first))>abs(*(minmaxRxx.second)))?abs(*(minmaxRxx.first)):abs(*(minmaxRxx.second));
+	double Rxx_coef = (appcorxx.end_y() - appcorxx.middle_y()) / Rxx_offs;
 	for(int i=0; i<VAR_DISCR; i++) {
-		appcorxx.out(t_arr[i]*t_coef, appcorxx.real_y(Rp_arr[i]*Rp_coef));
+		appcorxx.out(t_arr[i]*t_coef, appcorxx.real_y(Rxx_arr[i]*Rxx_coef));
 		SDL_SetRenderDrawColor(appcorxx.ren, 10, 150, 0, 0);
 		if(i+1<VAR_DISCR)
-			SDL_RenderDrawLine(appcorxx.ren, t_arr[i]*t_coef, appcorxx.real_y(Rp_arr[i]*Rp_coef), (t_arr[i+1]*t_coef), appcorxx.real_y(Rp_arr[i+1]*Rp_coef));
+			SDL_RenderDrawLine(appcorxx.ren, t_arr[i]*t_coef, appcorxx.real_y(Rxx_arr[i]*Rxx_coef), (t_arr[i+1]*t_coef), appcorxx.real_y(Rxx_arr[i+1]*Rxx_coef));
 	}
 	appcorxx.refresh_win();
 
-	// get xy cor
-	double Rxy=0;
 
-	for(int i=0; i<VAR_DISCR/2; i++) {
-		for(int j=0; j<VAR_DISCR/2; j++) {
-			 Rxy += (x_arr[i] - Mx) * (y_arr[j] - My);
+	// get correlation
+	for(int tau=0; tau<VAR_DISCR; tau++) {
+		Rxy_arr[tau] = 0;
+		for(int i=tau; i<VAR_DISCR-tau; i++) {
+			 double Rxy = (x_arr[i] - Mx) * (y_arr[i+tau] - Mx);
+			 Rxy_arr[tau] += Rxy;
 		}
+		//Rxx_arr[i] /= (VAR_DISCR/2-1);
+		printf("Rxy[%i]=%f\n\r", tau, Rxy_arr[tau]);
 	}
+
+	// draw Rxx(t)
+	std::pair<double*, double*> minmaxRxy = std::minmax_element(std::begin(Rxy_arr), std::end(Rxy_arr));
+	//std::pair<int*, int*> minmaxt = std::minmax_element(std::begin(t_arr), std::end(t_arr));
+	// conv
+	double Rxy_offs = (abs(*(minmaxRxy.first))>abs(*(minmaxRxy.second)))?abs(*(minmaxRxy.first)):abs(*(minmaxRxy.second));
+	double Rxy_coef = (appcorxy.end_y() - appcorxy.middle_y()) / Rxy_offs;
+	for(int i=0; i<VAR_DISCR; i++) {
+		appcorxy.out(t_arr[i]*t_coef, appcorxy.real_y(Rxy_arr[i]*Rxy_coef));
+		SDL_SetRenderDrawColor(appcorxy.ren, 10, 150, 0, 0);
+		if(i+1<VAR_DISCR)
+			SDL_RenderDrawLine(appcorxy.ren, t_arr[i]*t_coef, appcorxy.real_y(Rxy_arr[i]*Rxy_coef), (t_arr[i+1]*t_coef), appcorxy.real_y(Rxy_arr[i+1]*Rxy_coef));
+	}
+	appcorxy.refresh_win();
+
+
+	// get xy cor
 	// write results to file
 	ofstream file;
 	file.open("lab_res.txt");
@@ -162,7 +184,6 @@ int main(int argc, char **argv) {
 	file << "my time=" << my_timeused << endl;
 	file << "My=" << My << endl;
 	file << "Dy=" << Dy << endl;
-	file << "Rxx=" << Rxx_result << endl;
 	//file << "Rxy=" << Rxy_result << endl;
 	file.close();
 	// get it into google drive
